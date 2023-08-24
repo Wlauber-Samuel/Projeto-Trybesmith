bd0866944110930d379b60e76c3e49eaf0e00a4f
Wlauber-Samuel <wlaubersamuel@gmail.com>
2023-08-24 15:29:42 -0300

    consertando lint

--- src/controller/ordersController.ts
+++ src/controller/ordersController.ts
@@ -2,9 +2,9 @@ import { Request, Response } from 'express';
 import orderServices from '../services/ordersServices';
 
 const getAllOrders = async (_req: Request, res: Response): Promise<void> => {
-    const orders = await orderServices();
+  const orders = await orderServices();
 
-    res.status(200).json(orders);
+  res.status(200).json(orders);
 };
 
 export default getAllOrders;
