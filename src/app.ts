47f640e8cdf36090972d0146dea06fff165328a4
Wlauber-Samuel <wlaubersamuel@gmail.com>
2023-08-28 18:44:12 -0300

    requisito 4 concluído

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import productRouter from './routes/products.router';
 import orderRouter from './routes/orders.router';
+import loginRouter from './routes/login.router';
 
 const app = express();
 
 app.use(express.json());
 app.use(productRouter);
 app.use(orderRouter);
+app.use(loginRouter);
 
 export default app;
