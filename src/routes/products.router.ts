e653873aa635f0fcf68f3b9ba2a8cadaee9ac8cc
Wlauber-Samuel <wlaubersamuel@gmail.com>
2023-08-28 19:10:21 -0300

    requisito 5 concluído

--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import productController from '../controllers/product.controller';
+import nameMiddleware from '../middlewares/name.middleware';
+import priceMiddleware from '../middlewares/price.middleware';
 
 const productRouter = Router();
 
-productRouter.post('/products', productController.create);
+productRouter.post('/products', nameMiddleware, priceMiddleware, productController.create);
 
 productRouter.get('/products', productController.findAll);
 
